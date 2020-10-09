e68c60581af65e1ebc23d89d3aa1f1f5bb26597d
Sean Willess <swilless@SeanW.lan>
2020-10-09 11:58:20 -0400

    initial commit redo

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <WebTool/>
+  </div>
+</template>
+
+<script>
+import WebTool from './components/WebTool'
+
+export default {
+  name: 'App',
+  components: {
+    WebTool
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.button {
+  cursor: pointer;
+  color: white;
+  font-weight: bold;
+  background: blue;
+  padding: 10px 15px;
+  border-radius: 10px;
+}
+</style>
---
+++ src/components/WebTool.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <intro v-if="introLoad" @close="goToLocation()"></intro>
+    <locationPrompt v-if="location"></locationPrompt>
+  </div>
+</template>
+
+<script>
+import Intro from './Intro'
+import LocationPrompt from './LocationPrompt'
+
+export default {
+  name: 'WebTool',
+  data () {
+    return {
+      isMobile: false,
+      introLoad: true,
+      prompted: true,
+      location: false,
+      years: false
+    }
+  },
+  components: {
+    Intro, LocationPrompt
+  },
+  mounted() {
+    // load intro
+    if(this.introLoad){
+      this.introLoad = true
+    }
+  },
+  methods: {
+    goToLocation() {
+      this.introLoad = false
+      this.location = true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+  span {
+    color: red;
+  }
+}
+</style>
